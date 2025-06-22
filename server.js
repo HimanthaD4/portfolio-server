42bca4bc54fb05da52ea6ef5f4663b2e05ba1303
Himantha Hirushan <himanthad4@gmail.com>
2025-06-23 01:48:41 +0530

    install redis

--- server.js
+++ server.js
@@ -75,6 +75,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`🚀 Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
